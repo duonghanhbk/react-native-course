5d673c32dbf3ac5bf1d86c1a082972b7017676da
v.hanhnd6 <v.hanhnd6@vinid.net>
2020-06-30 15:08:11 +0700

    Init

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import HomeScreen from "./src/screens/HomeScreen";
+import DetailScreen from "./src/screens/Detail";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+after ad
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
